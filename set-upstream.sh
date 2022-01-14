854e57edab22e2fa718bb0a72783a72ffeb5b356
Jonas Kvarnström <jonas.kvarnstrom@liu.se>
2022-01-14 12:46:10 +0100

    Updated name of default branch.

--- set-upstream.sh
+++ set-upstream.sh
@@ -5,13 +5,12 @@
 # Add the original "forked" repository with the name "upstream"
 git remote add upstream git@gitlab.liu.se:jonkv82/javaoo-base.git
 
-# Check out the current master branch
-# TODO: Changes from master to main in release 14.0, shipping on June 22, 2021
-git checkout master
+# Check out the current main branch
+git checkout main
 
-# Fetch any new commits from the master branch of the "upstream" repository
+# Fetch any new commits from the main branch of the "upstream" repository
 git fetch upstream
 
-# Merge those commits into the current master branch.
+# Merge those commits into the current main branch.
 # This should result in you having to write a merge commit message.
-git pull upstream master
+git pull upstream main
